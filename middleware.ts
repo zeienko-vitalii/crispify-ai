4dd477deb498e6bd4b7ddfbc1ee9da3275f4076f
Vitaly Zeenko <dev.zeenko@gmail.com>
2024-02-26 15:03:10 +0100

    Update authMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/api/webhook/clerk"],
-	ignoredRoutes: ["/api/webhook/clerk"],
+	publicRoutes: ["/", "/api/webhooks/clerk"],
+	ignoredRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
